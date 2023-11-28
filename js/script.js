76be167eef4943002b34b18d0f1cd8eacbe72f4e
Md Hashmi <mdhashmi0711@gmail.com>
2023-11-28 20:42:51 +0530

    Make Responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const day = document.getElementById("day");
-const month = document.getElementById("month");
-const year = document.getElementById("year");
+const dayInput = document.getElementById("day");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
 const calculateAgeButton = document.querySelector(".arrow-div");
 const dayInvalidMsg = document.querySelector(".day-invalid-msg");
 const monthInvalidMsg = document.querySelector(".month-invalid-msg");
@@ -9,62 +9,61 @@ const dayContainer = document.querySelector(".day-dashes");
 const monthContainer = document.querySelector(".month-dashes");
 const yearContainer = document.querySelector(".year-dashes");
 
+const validateInput = (value, minValue, maxValue, element, errorMsgElement, errorMsg) => {
+    if(value < minValue || value > maxValue) {
+        element.style.borderColor = "red";
+        errorMsgElement.innerHTML = errorMsg;
+        return false;
+    }else {
+        element.style.borderColor = "";  
+        errorMsgElement.innerHTML = ""; 
+        return true;
+    }
+    
+}
 
-
-let date = new Date();
-
-let currentDay = date.getDate();
-let currentMonth = date.getMonth() + 1;
-let currentYear = date.getFullYear();
-
-let currentDate = `${day}-${month}-${year}`;
-console.log(currentDate);
+const calculateAndDisplayAge = (userValue, currentValue, container) => {
+    const difference = currentValue > userValue ? currentValue - userValue : userValue - currentValue;
+    const displayValue = difference < 10 ? "0" + difference : difference;
+    container.innerHTML = displayValue;
+}
 
 
 calculateAgeButton.addEventListener('click', () => {
-    if (!day.value || !month.value || !year.value) {
-        alert("Please fill all fields")
-    } else {
 
-        if (day.value < 0 || day.value > 31) {
-            day.style.borderColor = "red";
-            dayInvalidMsg.innerHTML = `Must be a valid day`;
-        } else {
-            let calculateDate = currentDay > Number(day.value)? currentDay - Number(day.value) : Number(day.value) - currentDay;
+    dayInvalidMsg.innerHTML = "";
+    monthInvalidMsg.innerHTML = "";
+    yearInvalidMsg.innerHTML = "";
 
-            if(calculateDate < 10) {
-                 dayContainer.innerHTML = "0" + calculateDate ;
-            }else {
-                dayContainer.innerHTML = calculateDate;
-            }
-        }
+    // const userDate = new Date(`${yearInput.value}-${monthInput.value}-${dayInput.value}`);
+    // const userDay  = userDate.getDate();
+    // const userMonth = userDate.getMonth() + 1;
+    // const userYear = userDate.getFullYear();
 
-        if (month.value < 0 || month.value > 12) {
-            month.style.borderColor = "red";
-            monthInvalidMsg.innerHTML = `Must be a valid month`;
-        } else {
-            let calculateMonth = currentMonth > Number(month.value) ? currentMonth - Number(month.value) : Number(month.value) - currentMonth;
-            
-            if(calculateMonth < 10) {
-                monthContainer.innerHTML = "0" + calculateMonth;
-            }else {
-                monthContainer.innerHTML = calculateMonth;
-            }
-        }
+    const userDay = parseInt(dayInput.value, 10);
+    const userMonth = parseInt(monthInput.value, 10);
+    const userYear = parseInt(yearInput.value, 10);
 
-        if (year.value < 1800 || year.value > currentYear) {
-            year.style.borderColor = "red";
-            if(year.value > currentYear) {
-                yearInvalidMsg.innerHTML = `Must be in the past`;
-            } else if( year.value < 1800) {
-                yearInvalidMsg.innerHTML = `Add after 1800`;
-            }
-            
-        }else {
-            yearContainer.innerHTML = currentYear - Number(year.value);
-        }
+
+     if (!dayInput.value || !monthInput.value || !yearInput.value) {
+        alert("Please fill in all fields.");
+        return;
     }
 
+    if (isNaN(userDay) || isNaN(userMonth) || isNaN(userYear)) {
+        alert("Please enter valid numbers for day, month, and year.");
+        return;
+    }
+
+    const isDayValid = validateInput(userDay, 1, 31, dayInput, dayInvalidMsg, 'Must be a valid day');
+    const isMonthValid = validateInput(userMonth, 1, 12, monthInput, monthInvalidMsg, 'Must be a valid month');
+    const isYearValid = validateInput(userYear, 1800, new Date().getFullYear(), yearInput, yearInvalidMsg, 'Must be in the past');
+
+    if(isDayValid && isMonthValid && isYearValid) {
+        calculateAndDisplayAge(userDay, new Date().getDate(), dayContainer);
+        calculateAndDisplayAge(userMonth, new Date().getMonth() + 1, monthContainer);
+        calculateAndDisplayAge(userYear, new Date().getFullYear(), yearContainer);
+    }
 });
 
 
